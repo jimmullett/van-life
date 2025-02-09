5db21baf2c9a8e0a8ce98973a09021f4234b1cb8
Jim Mullett <jimmullett720@gmail.com>
2025-02-08 20:52:02 -0700

    removed console log

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIRESTORE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIRESTORE_APP_ID
 };
-console.log(firebaseConfig)
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const vansRef = collection(db, "vans");
