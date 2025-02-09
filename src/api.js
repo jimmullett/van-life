64c086a48cc0cd1892d296590cf8e61ed8cf97a1
Jim Mullett <jimmullett720@gmail.com>
2025-02-08 19:59:13 -0700

    added firestore

--- src/api.js
+++ src/api.js
@@ -1,29 +1,42 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_MtmG-FxYDswyWnlOIIEd9GzjvEwcoqM",
+  authDomain: "vanlife-scrimba-d070a.firebaseapp.com",
+  projectId: "vanlife-scrimba-d070a",
+  storageBucket: "vanlife-scrimba-d070a.firebasestorage.app",
+  messagingSenderId: "495751074754",
+  appId: "1:495751074754:web:ddafdd326e8fe3080bfc5b"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const vansRef = collection(db, "vans");
+
+export async function getVans() {
+    const snapshot = await getDocs(vansRef);
+    const vans = snapshot.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id
+    }));
+    return vans;
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+  return { ...snapshot.data(), id: snapshot.id };
+}
+
+export async function getHostVans() {
+  const q = query(vansRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map(doc => ({
+    ...doc.data(),
+    id: doc.id
+  }));
+  return vans;
 }
 
 export async function loginUser(creds) {
